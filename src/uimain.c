f159d4031ff40d43d8989654f37ee809ee381a64
PatrickPR4 <131415840+PatrickPR4@users.noreply.github.com>
2023-10-03 21:09:36 -0600

    Update uimain.c

--- src/uimain.c
+++ src/uimain.c
@@ -2,47 +2,7 @@
 #include <stdlib.h>
 #include "tokenizer.h"
 #include "history.h"
-/*
-int main() {
-  List *history = init_history();
 
-  while(1) {
-    printf("Press 't' to tokenize\n");
-    printf("Press 'h' to view histories\n");
-    printf("Press 'n' followed by the number to recall a certain history item\n");
-    printf("Press 'q' to quit\n\n");
-    char user_selection[10];
-    fgets(user_selection, 10, stdin);
-
-    switch(user_selection[0]) {
-    case 't':
-      printf("Please enter a sentence(limit is 100 characters):\n>");
-      char input [100];
-      fgets(input, 100, stdin);
-      printf("\nYour sentence was: %s", input);
-      char **tokens = tokenize(input);
-      print_tokens(tokens);
-      add_history(history, input);
-      print_history(history);
-      break;
-    case 'h':
-      print_history(history);
-      break;
-    case 'n':
-      int number = atoi(user_selection+1);
-      printf("%d is: ", number);
-      printf("%s", get_history(history, number));
-      break;
-    case 'q':
-      return 0;
-    default:
-      printf("Invalid option");
-      break;
-    }
-    printf("\n");
-  }
-}
-*/
 
 int main() {
   List *history = init_history();
